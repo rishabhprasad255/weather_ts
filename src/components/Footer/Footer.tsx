84c0172158133aa77f25cbee84dde818f8242efc
rishabhprasad255 <rishabhprasad490@gmail.com>
2022-08-14 10:15:08 +0530

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FooterContainer } from "./styled";
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <p>Developed By RISHABH PRASAD</p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
