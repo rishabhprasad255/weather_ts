04c9233472d962354632603b105d66db88ced03e
rishabhprasad255 <rishabhprasad490@gmail.com>
2022-08-14 10:31:58 +0530

    commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { ReactComponent as GithubIcon } from '../../assets/github.svg';
-import { GithubLink, HeaderContainer, Title, HeaderIconsContainer } from './styed';
-import { useDispatch, useSelector } from 'react-redux';
-import DarkModeToggle from 'react-dark-mode-toggle';
-import { AppStore } from '../../store/store';
-import { toggleDarkMode } from '../../store/reducers/appReducer';
+import React from "react";
+import { ReactComponent as GithubIcon } from "../../assets/github.svg";
+import {
+  GithubLink,
+  HeaderContainer,
+  Title,
+  HeaderIconsContainer,
+} from "./styed";
+import { useDispatch, useSelector } from "react-redux";
+import DarkModeToggle from "react-dark-mode-toggle";
+import { AppStore } from "../../store/store";
+import { toggleDarkMode } from "../../store/reducers/appReducer";
 
 const Header: React.FC = () => {
   const dispatch = useDispatch();
@@ -14,8 +19,12 @@ const Header: React.FC = () => {
     <HeaderContainer>
       <Title>React Weather</Title>
       <HeaderIconsContainer>
-        <DarkModeToggle checked={isDarkMode} onChange={() => dispatch(toggleDarkMode())} size={60} />
-        <GithubLink href="http://www.github.com/esnz/reactweather">
+        <DarkModeToggle
+          checked={isDarkMode}
+          onChange={() => dispatch(toggleDarkMode())}
+          size={60}
+        />
+        <GithubLink href="https://github.com/rishabhprasad255/weather_ts">
           <GithubIcon />
         </GithubLink>
       </HeaderIconsContainer>
